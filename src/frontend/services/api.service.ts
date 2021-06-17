c0f8389b5db9143047fbac7cd1bfa5b12229704d
Vien Dinh <maxvien@gmail.com>
2021-06-17 11:04:36 +0700

    fix port

--- src/frontend/services/api.service.ts
+++ src/frontend/services/api.service.ts
@@ -4,7 +4,7 @@ import { EnvService } from '@app/shared/services/env.service';
 let baseURL = '/api';
 
 if (EnvService.isNode()) {
-  const port: number = +process.env.PORT | 3000;
+  const port: number = +process.env.PORT || 3000;
   baseURL = `http://localhost:${port}/api`;
 }
 
